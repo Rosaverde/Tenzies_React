99c347c714121d4fa21a28b43c99693745d0ccc3
Rosaverde <rosaverde.ag@gmail.com>
2023-06-28 23:54:21 +0000

    Prepared dices

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from "react";
+import Dice from "./Dice"
 import "./style.css"
 
 export default function App()
 {
     return(
         <main>
-            <h1>Test</h1>
+            <div className="board" > 
+                <Dice value="1"/>
+                <Dice value="2"/>
+                <Dice value="3"/>
+                <Dice value="4"/>
+                <Dice value="5"/>
+                <Dice value="5"/>
+                <Dice value="4"/>
+                <Dice value="3"/>
+                <Dice value="2"/>
+                <Dice value="1"/>
+            </div>
         </main>
     )
 }
